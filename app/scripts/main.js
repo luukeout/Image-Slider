798c413b4909bd23123de8b03374d8616ee0cd11
luke olson <luukeout@gmail.com>
2014-05-28 07:47:51 -0400

    image slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+var photos = ["http://i.imgur.com/zlTyybQb.jpg", "http://i.imgur.com/hWDF9DXb.jpg", "http://i.imgur.com/HbETGwsb.jpg"];
+
+
+
+
+var intervalID = window.setInterval(photos, 3000);
+
+function createSlideshow () {
+	photos.forEach(function (photo) {
+		$('.jumbotron').append("<img src=" + photo +"/>")
+	});
+};
+
+createSlideshow();
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,14 +1,36 @@
 /* global describe, it */
 
-(function () {
-    'use strict';
+// (function () {
+//     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-              var answer = 2;
-              expect(2).to.equal(answer);
-            });
-        });
-    });
+//     describe('Give it some context', function () {
+//         describe('maybe a bit more context here', function () {
+//             it('should run here few assertions', function () {
+//               var answer = 2;
+//               expect(2).to.equal(answer);
+//             });
+//         });
+//     });
+// })();
+
+// (function() {
+
+// 	describe("image-slider", function() {
+// 		it("should transition between images every 2 seconds.", function() {
+			
+// 			var display = new Display()
+// 			expect(display)
+// 		})
+
+// 	})
+// })
+(function() {
+'use strict';
+	describe("image-slider", function() {
+		it("should inject the correct # of img tags", function() {
+			var total = $('.slideshow-container img').length
+
+			expect(total).to.equal(2)
+		});
+	});
 })();
